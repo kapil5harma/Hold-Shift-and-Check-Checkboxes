0b25dff3239f072c87e129887fdbbc72f29e610f
Kapil Sharma <19823356+kapil5harma@users.noreply.github.com>
2018-04-04 00:02:59 +0530

    Completed Hold Shift and Check Checkboxes

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 console.log(checkboxes);
 
-function handleClick(e) {
-    console.log('e: ', e);
+let lastChecked;
 
+function handleClick(e) {
+  console.log("e: ", e);
+  let inBetween = false;
+
+  // Check if we have Shift key pressed.
+  // && Check that we are checking it.
+  if (e.shiftKey && this.checked) {
+    // Go ahead and do what we please.
+    // Loop over every single checkbox
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log("Starting to check them in between.");
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this;
 }
 
-checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleClick));
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener("click", handleClick);
+});
